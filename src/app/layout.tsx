a7666812cdc54bd262f91c2c284039adf7b13cae
hongkyokim <hongkyokim@bankx.co.kr>
2025-12-28 02:49:46 +0900

    ad센스 메타 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -186,6 +186,12 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1754856977895556"
+          crossOrigin="anonymous"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased noise-overlay`}
